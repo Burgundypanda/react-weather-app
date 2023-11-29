7f955331c439753084475041433f2359e410768b
Burgundypanda <108671429+Burgundypanda@users.noreply.github.com>
2023-11-29 14:52:29 +0000

    Added github link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by Jakiya Rahman and is{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Burgundypanda/react-weather-app"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          open-sourced on Githib
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
